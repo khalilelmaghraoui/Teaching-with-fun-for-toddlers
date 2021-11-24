b15c231b7313a8d832fcb9c3f4e297b0bbd2291c
khalil <elmaghraoui.khalil.1@gmail.com>
2021-11-24 21:48:09 +0100

    abcActivity

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView abs;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        abs = findViewById(R.id.abc);
+        abs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activity2Intent = new Intent(getApplicationContext(), abcActivity.class);
+                startActivity(activity2Intent);
+
+            }
+        });
     }
 }
