a78b51e02cefdd93d298ea6028ae89f355272eae
khalil <elmaghraoui.khalil.1@gmail.com>
2021-11-25 01:19:14 +0100

    done

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -10,16 +10,33 @@ import android.widget.ImageView;
 public class MainActivity extends AppCompatActivity {
 
     ImageView abs;
+    ImageView un23;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // buttons
         abs = findViewById(R.id.abc);
+        un23 = findViewById(R.id.un23);
+
+
+        //Listener abs
         abs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent activity2Intent = new Intent(getApplicationContext(), abcActivity.class);
+                startActivity(activity2Intent);
+
+            }
+        });
+
+        //Listener un123
+
+        abs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activity2Intent = new Intent(getApplicationContext(), un23Activity.class);
                 startActivity(activity2Intent);
 
             }
